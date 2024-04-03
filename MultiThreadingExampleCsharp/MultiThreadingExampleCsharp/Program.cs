f41ec4588b3c79db6e2cd9b825dec58c5dc7d741
Sisila <sisilajayamal1@gmail.com>
2024-04-03 17:18:37 +0530

    Initial commit

---
+++ MultiThreadingExampleCsharp/MultiThreadingExampleCsharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread newThread = new Thread(new ThreadStart(PrintNumbers));
+        newThread.Start();
+
+        Thread newThread2 = new Thread(new ThreadStart(PrintNames));
+        newThread2.Start();
+    }
+
+
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Thread 1: " + i);
+            Thread.Sleep(500);
+        }
+    }
+
+    static void PrintNames()
+    {
+        String[] Names = { "Sisila", "Shaki", "Shashi", "Sisi", "Sunny" };
+
+        for (int i = 0; i <= 4; i++)
+        {
+            Console.WriteLine("Thread 2: " + Names[i]);
+            Thread.Sleep(500);
+        }
+
+    }
+
+}
+
+
